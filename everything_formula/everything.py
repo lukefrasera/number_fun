9c68485a536912f51afc74cda48d74ffd842520b
Luke A Fraser <lukefrasera@gmail.com>
2017-04-01 16:47:39 -0700

    adding everything

---
+++ everything_formula/everything.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import os
+import argparse
+
+
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit((0 or main()))
